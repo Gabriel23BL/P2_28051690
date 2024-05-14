c64e7dcbeac3ac495c831656a984bb350862cdf8
Gabriel23BL <165972978+Gabriel23BL@users.noreply.github.com>
2024-05-14 11:58:36 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,26 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+const sqlite = require("sqlite3");
+let sql="";
+
+const db = new sqlite.Database('./data.db', sqlite.OPEN_READWRITE, (error) =>{
+  if(error){
+    console.error(error);
+  }
+});
+sql = `CREATE TABLE IF NOT EXISTS contactos (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  email VARCHAR(50) NOT NULL,
+  nombre VARCHAR(50) NOT NULL,
+  comentario TEXT,
+  ip TEXT NOT NULL,
+  fecha_hora TEXT
+)`;
+db.run(sql, (error) => {
+  if(error){
+    console.error(error);
+  }
+});
+
