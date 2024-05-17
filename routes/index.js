cbbae5aeac03f004d8079aa5aa2bf6f76d8fde3d
Gabriel23BL <165972978+Gabriel23BL@users.noreply.github.com>
2024-05-17 17:31:48 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
+const ContactosController = require("../controllers/ContactosController");
+const contactosController = new ContactosController();
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CV SEBASTIAN RAMIREZ' });
 });
+
+router.post("/form-contacto", contactosController.add);
 
 module.exports = router;
