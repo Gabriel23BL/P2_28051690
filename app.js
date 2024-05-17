1a29a46805e05adaaaccc1b88580b1c71f8d71b0
Gabriel23BL <165972978+Gabriel23BL@users.noreply.github.com>
2024-05-17 08:04:19 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var contactRouter = require('./routes/contact');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+
 var app = express();
 
+
+/* const db = require('./conf/db'); */
+const ContactosController = require('./ContactosController');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -17,10 +21,12 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/contact', contactRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -41,49 +47,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-
-//sql
-const sqlite = require("sqlite3");
-const db = new sqlite.Database('./data.db', sqlite.OPEN_READWRITE, (error) => {
-  if (error) {
-    console.error(error);
-  }
-});
-
-const createTableQuery = `CREATE TABLE IF NOT EXISTS contactos (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  email VARCHAR(50) NOT NULL,
-  nombre VARCHAR(50) NOT NULL,
-  comentario TEXT,
-  ip TEXT NOT NULL,
-  fecha_hora TEXT
-)`;
-
-db.run(createTableQuery, (error) => {
-  if (error) {
-    console.error(error);
-  }
-});
-
-const insertQuery = `INSERT INTO contactos (email, nombre, comentario, ip, fecha_hora) VALUES (?, ?, ?, ?, ?)`;
-
-const email = "example@example.com";
-const nombre = "nombre";
-const comentario = "Este es un comentario";
-const ip = "127.0.0.1";
-const fecha_hora = new Date().toISOString();
-
-db.run(insertQuery, [email, nombre, comentario, ip, fecha_hora], function (error) {
-  if (error) {
-    console.error(error.message);
-  } else {
-    console.log(`Contacto con ID ${this.lastID} guardado correctamente`);
-  }
-});
-
-db.close((error) => {
-  if (error) {
-    console.error(error);
-  }
-});
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
-var express = require('express');
+const ContactosController = require("./ContactosController");
+const contactosController = new ContactosController();
+
+const indexController = require("./indexController")
+
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", indexController);
+
+router.post("/form-contacto", contactosController.add);
 
 module.exports = router;
-
